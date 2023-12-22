918d2da6297dd9ee6ff516bba0a3b2bbea2a8a6c
Philippe Faist <phfaist@gmail.com>
2023-12-22 22:42:59 +0100

    working on search tools

---
+++ zoodbsearch/searchindex.js
@@ -0,0 +1,220 @@
+import debug_module from 'debug';
+const debug = debug_module('zoodbtools_search.searchindex');
+
+import lunr from 'lunr';
+
+
+
+export function lunrPluginAdvancedSetup(builder)
+{
+
+}
+
+
+
+/**
+ * An object holding an index for the zoo database, which can be used for
+ * searching with the `Lunr` library.
+ *
+ * The SearchIndex assumes that you have processed your database using the
+ * :class:`SearchableTextProcessor` database processor.
+ *
+ * TODO Doc.......... explain how this works!
+ */
+export class SearchIndex
+{
+    static create(zoodb, searchable_text_fieldset, options)
+    {
+        debug(`setting up search index`);
+
+        options ??= {};
+
+        let fields_options = options.fields_options ?? {};
+        fields_options.boost = Object.assign(
+            { [searchable_text_fieldset.field_name_title]: 40, },
+            fields_options.boost ?? {}
+        );
+
+        const info = {
+            advanced_setup: options.advanced_setup ?? true,
+            object_types: searchable_text_fieldset.object_types,
+            fields: searchable_text_fieldset.fields,
+            field_name_id: searchable_text_fieldset.field_name_id,
+            field_name_title: searchable_text_fieldset.field_name_title,
+            fields_options,
+        };
+
+        //
+        // create the store of all documents' searchable text documents
+        //
+        debug(`creating text data store ...`);
+        let store = [];
+        for (const object_type of searchable_text_fieldset.object_types) {
+            const object_db = zoodb.objects[object_type];
+            for (const [object_id, object] of Object.entries(object_db)) {
+
+                if (object._zoodb == null) { // null or undefined
+                    throw new Error(`${object_type} ${object_id} does not have field _zoodb!`);
+                }
+
+                const searchable_text_doc =
+                      object._zoodb[ searchable_text_fieldset.searchable_text_fieldset_name ];
+
+                if (searchable_text_doc == null) { // null or undefined
+                    throw new Error(
+                        `${object_type} ${object_id}'s _zoodb does not have the field `
+                            + `${searchable_text_fieldset.searchable_text_fieldset_name}!`
+                    );
+                }
+
+                const doc = Object.assign(
+                    searchable_text_doc,
+                    { _z_stid: store.length } // lunr needs the current index here.
+                );
+
+                store.push(doc);
+
+            }
+        }
+        debug(`... done.`);
+
+        return new SearchIndex(info, store, null);
+    }
+
+    build()
+    {
+        let info = this.info;
+        let store = this.store;
+
+        //
+        // build the index!
+        //
+        debug(`building the index ...`);
+        this.idx = lunr( function () {
+            //
+            // Function will build the index. Lunr's API is accessed via 'this'
+            // (aliased to 'builder' here).
+            //
+            let builder = this;
+
+            builder.ref('_z_stid');
+            builder.field('_z_otype');
+            builder.field('_z_href');
+            // these are included in info.fields
+            // builder.field(this.field_name_id);
+            // builder.field(this.field_name_title);
+            for (const fldname of info.fields) {
+                const { boost } = info.fields_options[fldname] ?? {};
+                if (boost != null) {
+                    builder.field(fldname, {boost: boost});
+                } else {
+                    builder.field(fldname);
+                }
+            }
+            builder.metadataWhitelist = [ 'position' ];
+
+            if (info.advanced_setup) {
+                builder.use(lunrPluginAdvancedSetup);
+            }
+            
+            for (const doc of store) {
+                //debug(`Adding doc =`, doc);
+                builder.add(doc);
+            }
+
+        } );
+        debug(`... done.`);
+    }
+
+    static load(search_index_data)
+    {
+        const {info, serialized_store, serialized_index} = search_index_data;
+
+        const store = this._load_store(info, serialized_store);
+
+        if (serialized_index != null) {
+            const idx = this._load_idx_notnull(info, serialized_index);
+            return new SearchIndex(info, store, idx);
+        } else {
+            let si = new SearchIndex(info, store, null);
+            si.build();
+            return si;
+        }
+    }
+
+    toJSON()
+    {
+        return {
+            info: this.info,
+            serialized_store: this._dump_store(),
+            serialized_index: this._dump_idx(),
+        };
+    }
+
+
+    // you shouldn't have to use this directly
+    constructor(info, store, idx)
+    {
+        this.info = info;
+        this.store = store;
+        this.idx = idx;
+    }
+
+
+    //
+    // internal methods to load/dump index and store.
+    //
+    _dump_idx()
+    {
+        if (this.idx == null) {
+            return null;
+        }
+        return this.idx.toJSON();
+    }
+
+    static _load_idx_notnull(info, serialized_index)
+    {
+        return lunr.Index.load( serialized_index );
+    }
+
+    _dump_store()
+    {
+        // compress the store's representation to save data
+        const store_size = this.store.length;
+        const storefields = [].concat(['_z_otype', '_z_href'], this.info.fields);
+        let serialized_store = Object.fromEntries(
+            storefields
+            .map( (fldname) => [fldname, new Array(store_size)] )
+        );
+        for (let j = 0; j < store_size; ++j) {
+            const st_doc = this.store[j];
+            if (j != st_doc._z_stid) {
+                throw new Error(
+                    `Internal error: inconsistency of store id: `
+                    + `j=${j}, st_doc=${JSON.stringify(st_doc)}`
+                );
+            }
+            for (const fieldname of storefields) {
+                serialized_store[fieldname][j] = st_doc[fieldname];
+            }
+        }
+        serialized_store._z_store_size = store_size;
+        return serialized_store;
+    }
+    static _load_store(info, serialized_store)
+    {
+        const storefields = [].concat(['_z_otype', '_z_href'], info.fields);
+        const store_size = serialized_store._z_store_size;
+        let store = new Array(store_size);
+        for (let j = 0; j < store_size; ++j) {
+            store[j] = Object.fromEntries(
+                storefields.map(
+                    (fieldname) => [fieldname, serialized_store[fieldname][j]]
+                )
+            );
+            store[j]._z_stid = j;
+        }
+        return store;
+    }
+
+}
---
+++ zoodbsearch/searchwidget.js
@@ -0,0 +1,462 @@
+import debug_module from 'debug';
+const debug = debug_module('zoodbtools_search.searchwidget');
+
+import lunr from 'lunr';
+
+import tippy from 'tippy.js';
+
+// REQUIRE CUSTOM STYLING !!
+// import 'tippy.js/dist/tippy.css'; // for styling
+// import 'tippy.js/themes/light.css';
+
+import './searchwidget.scss';
+
+const default_context_length = 50; // chars
+
+const max_num_results_for_mathjax = 100;
+
+
+
+// const _html_escapes = {
+//     '&': '&amp;',
+//     '"': '&quot;',
+//     "'": '&#39;';
+//     '<': '&lt;',
+//     '>': '&gt;',
+// };
+// const _html_esc_rx = /[&"'<>]/g;
+//
+// function escape_html(str)
+// {
+//     return str.replace( _html_esc_rx, (matchstr) => _html_escapes[matchstr] );
+// }
+
+
+
+function escape_html(str)
+{
+    let p = document.createElement("p");
+    p.appendChild(document.createTextNode(str));
+    return p.innerHTML;
+}
+
+
+function default_resolve_href(object_type, object_id, doc) {
+    return doc._z_href;
+}
+
+
+/**
+ * Manage an HTML widget that can search contents in the zoo, given an index
+ * object (see :class:`SearchIndex`).
+ *
+ * The `search_index` argument is the :class:`SearchIndex` instance.
+ *
+ * The `options` argument is an object which can contain the following keys:
+ *
+ * - ``dom_container:`` the DOM container (e.g., returned by
+ *   `document.getElementById()`) where to install the search widget.  If a
+ *   string is given here and it begins with '#', then it is assumed to specify
+ *   the ID of a DOM container which is looked up using
+ *   `document.getElementById()`.
+ *
+ * - ``initial_search_query:`` a search query to initialize the widget with.
+ *   Set to null, false, or an empty string to start with a blank widget.
+ *
+ * - ``auto_fuzz_min_word_length:`` Auto-fuzz will apply a fuzziness tolerance
+ *   (edit distance tolerance) to all search terms that have a minimal length.
+ *   Set `auto_fuzz_min_word_length` to specify the search term length starting
+ *   from which we apply an automatic edit distance tolerance.
+ *
+ * - ``auto_fuzz_distance:`` Auto-fuzz will apply a fuzziness tolerance (edit
+ *   distance tolerance) to all search terms that have a minimal length.  Set
+ *   `auto_fuzz_distance` to specify the edit distance tolerance that is
+ *   automatically enabled for search terms that whose length is at least
+ *   `auto_fuzz_min_word_length`.
+ *
+ * - ``context_length:`` The number of characters of content to include in the
+ *   search results before, and after, occurrences of the search term in
+ *   documents.
+ *
+ * - ``resolve_href:`` a callback accepting arguments `(object_type, object_id,
+ *   doc)` that returns an URL where we should go to if we want to look up the
+ *   given object specified by its type and id.  (The `doc` argument provides
+ *   the stored field values of that object, which we used for the search.)
+ *
+ * - ``getMathJax:`` to support MathJax, set this property to a function that
+ *   returns a MathJax instance.
+ *
+ */
+export class SearchWidget
+{
+    constructor(search_index, options = {})
+    {
+        this.search_index = search_index;
+
+        this.resolve_href = options.resolve_href ?? default_resolve_href
+
+        this.dom_container = options.dom_container;
+        if (typeof this.dom_container === 'string' && this.dom_container.startswith('#')) {
+            this.dom_container = window.document.getElementById(this.dom_container.slice(1));
+        }
+
+        this.initial_search_query = options.initial_search_query;
+
+        this.auto_fuzz_min_word_length = options.auto_fuzz_min_word_length ?? 4;
+        this.auto_fuzz_distance = options.auto_fuzz_distance ?? 1;
+        debug("Auto fuzz distance is = ",
+              this.auto_fuzz_distance,
+              " to be applied to words of length at least = ",
+              this.auto_fuzz_min_word_length);
+
+        this.context_length = options.context_length ?? default_context_length;
+
+        this.getMathJax = options.getMathJax ?? null;
+
+        this.tippyAppearanceTheme = options.tippyAppearanceTheme ?? null;
+
+        this._install();
+    }
+
+    _install()
+    {
+        //
+        // Install on web page
+        //
+
+        console.log("Installing the search widget in the page.");
+
+        // clear any existing content in the target app container
+        this.dom_container.innerHTML = '';
+
+        // set base CSS class
+        this.dom_container.classList.add('zoodb-search-widget');
+
+        // create the input text box
+        const inputbox = document.createElement('input');
+        inputbox.setAttribute('type', "text");
+        inputbox.classList.add("search-input");
+        inputbox.value = (this.initial_search_query || '');
+        inputbox.placeholder = 'type & hit RETURN to search';
+        this.dom_container.appendChild(inputbox);
+
+        this.search_input = inputbox;
+
+        // create the results pane
+        const divresults = document.createElement('div');
+        divresults.classList.add('search-results');
+        this.dom_container.appendChild(divresults);
+
+        this.search_results_container = divresults;
+
+        // latch on to the input's "change" event
+        this.search_input.addEventListener(
+            'change',
+            (event) => this._do_search_event(event)
+        );
+        
+        // create a simple instructions widget
+        let display_fields_html =
+            this.search_index.info.fields.filter( (fld) => (fld.substr(0,1) != '_') )
+            .map( (fld) => '<code>'+fld+'</code>' )
+            .join(', ');
+        let instructions_widget = document.createElement('div');
+        instructions_widget.classList.add('search-instructions-tip');
+        instructions_widget.innerHTML = 
+           `<p>Type query term(s) and hit RETURN to search. </p>
+            <p>Advanced usage:</p>
+            <p class="search-instructions-item">
+                <code>+term</code> or <code>-term</code> to force the presence
+                or the abscence of a term;
+            </p>
+            <p class="search-instructions-item">
+              <code>field:term</code> to restrict the search term to within
+              the given <i>field</i>. Possible fields: ${display_fields_html}.
+            </p>
+            <p class="search-instructions-item">
+              <code>term~4</code> to include all terms
+              with edit distance 4 from the given term (use
+              <code>term~0</code> to cancel default fuzziness);
+            </p>
+            <p class="search-instructions-item">
+              <code>term^10</code> to &quot;boost&quot; the term, i.e., to make
+              it contribute more to the final match score.
+            </p>`;
+        this.dom_container.appendChild(instructions_widget);
+
+        let moreTippyOptions = {};
+        if (this.tippyAppearanceTheme) {
+            moreTippyOptions.theme = this.tippyAppearanceTheme;
+        }
+
+        // add a tippy widget for some simple instructions
+        this.tippy_instance = tippy(
+            this.search_input,
+            {
+                content: instructions_widget,
+                allowHTML: true,
+                trigger: 'click',
+                interactive: true,
+                interactiveBorder: 30,
+                maxWidth: '450px',
+                placement: 'bottom',
+
+                ... moreTippyOptions
+            }
+        );
+
+        if (this.initial_search_query) {
+            this.do_search(this.initial_search_query);
+        }
+    }
+
+    _do_search_event(event)
+    {
+        const value = event.target.value;
+        this.do_search(value);
+    }
+
+    do_search(search_str)
+    {
+        if (this.getMathJax != null) {
+            this.getMathJax()?.typesetClear(this.search_results_container);
+        }
+        this.search_results_container.innerHTML = '';
+
+        this.tippy_instance.hide();
+
+        if (!search_str || search_str.trim().length == 0) {
+            this._display_search_no_query_string();
+            return;
+        }
+
+        console.log("Searching! search_str =", search_str);
+
+        let _auto_fuzz_min_word_length = this.auto_fuzz_min_word_length;
+        let _auto_fuzz_distance = this.auto_fuzz_distance;
+
+        let results;
+        try {
+
+            let q = function (query) {
+                let parser = new lunr.QueryParser(search_str, query);
+                let qq = parser.parse();
+                console.log("Query = ", qq);
+                // tweak the query to add an edit distance to all terms
+                for (let clause of qq.clauses) {
+                    console.log("Processing clause: ", clause,
+                                " auto_fuzz_min_word_length = ", _auto_fuzz_min_word_length,
+                                " auto_fuzz_distance = ", _auto_fuzz_distance);
+                    let term_length = clause.term.length;
+                    if (clause.term.charAt(0) == '*') { --term_length; }
+                    if (clause.term.charAt(clause.term.length - 1) == '*') { --term_length; }
+                    if (typeof clause.editDistance === 'undefined'
+                        && term_length >= _auto_fuzz_min_word_length) {
+                        clause.editDistance = _auto_fuzz_distance;
+                    }
+                }
+                console.log("Done processing clauses.");
+                return qq;
+            };
+
+            results = this.search_index.idx.query(q);
+
+        } catch (e) {
+            if (e instanceof lunr.QueryParseError) {
+                this._display_search_error(search_str, e);
+                return
+            } else {
+                throw e;
+            }
+        }
+
+        console.log('results =', results);
+
+        this._display_search_results(search_str, results);
+    }
+
+    _display_search_no_query_string()
+    {
+        const p = document.createElement('p');
+        p.classList.add('search-no-query-string');
+        p.textContent =
+            'Type one or more words above and hit “RETURN” to search.';
+
+        this.search_results_container.appendChild(p);
+    }
+
+    _display_search_error(search_str, error)
+    {
+        console.error("Error in search.", error);
+
+        const p = document.createElement('p');
+        p.classList.add('search-results-error');
+        
+        const s1 = document.createElement('span');
+        s1.textContent = 'Error in search query ‘'+search_str+'’: ';
+        p.appendChild(s1);
+        
+        const s2 = document.createElement('span');
+        s2.classList.add('search-results-error-message');
+        s2.textContent = error.message;
+        p.appendChild(s2);
+        
+        this.search_results_container.appendChild(p);
+    }
+
+    _display_search_results(search_str, results)
+    {
+        if (results.length == 0) {
+            this._display_search_no_results(search_str);
+        } else {
+            results.forEach(
+                (r) => this._add_display_result(r, this.search_results_container)
+            );
+
+            if ( (results.length < max_num_results_for_mathjax)
+                 && this.getMathJax != null) {
+
+                const MathJax = this.getMathJax();
+                if (MathJax != null) {
+                    // reset equation numbering & disable numbers (to avoid
+                    // potentially multiply-defined labels)
+                    MathJax.texReset();
+
+                    // Setting tags to null here doesn't work, probably need to do
+                    // this before loading MathJax
+                    //
+                    //this.getMathJax().config.tex.tags = null;
+
+                    // typeset the math in our results
+                    MathJax.typesetPromise();
+                }
+            }
+        }
+    }
+
+    _display_search_no_results(search_str)
+    {
+        const p = document.createElement('p');
+        p.classList.add('search-no-results');
+        p.textContent = `Your search for ‘${search_str}’ did not yield any results.`;
+
+        this.search_results_container.appendChild(p);
+    }
+
+
+    _add_display_result(result, container)
+    {
+        const doc = this.search_index.store[parseInt(result.ref)];
+
+        const div = document.createElement('div');
+        div.classList.add("search-result");
+
+        let srt = document.createElement('div');
+        srt.classList.add("search-result-title");
+        div.appendChild(srt);
+        let a = document.createElement('a');
+        const doc_id = doc[this.search_index.info.field_name_id];
+        a.setAttribute('href', this.resolve_href(doc._z_otype, doc_id, doc));
+        a.classList.add("search-result-link");
+        srt.appendChild(a);
+        a.innerText = doc[this.search_index.info.field_name_title];
+
+        const hipos = {}; // hipos[field] = [ (list of highlight positions) ]
+
+        for (const [/*word*/, wordmatches] of Object.entries(result.matchData.metadata)) {
+            for (const [fieldname, fieldmatches] of Object.entries(wordmatches)) {
+
+                const poslist = fieldmatches.position;
+                //console.log('word =', word, 'fieldname =', fieldname, 'poslist =', poslist);
+
+                if ( ! Object.hasOwn(hipos, fieldname) ) {
+                    hipos[fieldname] = [];
+                }
+
+                hipos[fieldname] = hipos[fieldname].concat(poslist);
+            }
+        }
+
+
+        const context_length = this.context_length;
+
+        // iterate over this.search_index.info.fields instead, to preserve field order
+        for (const fieldname of this.search_index.info.fields) {
+
+            if ( ! Object.hasOwn(hipos, fieldname) ) {
+                // no such field in match
+                continue;
+            }
+
+            const poslist = hipos[fieldname];
+
+            // sort highlight positions by starting position
+            poslist.sort( (a, b) => a[0] - b[0] );
+            // make sure that end position does not overstep another starting position
+            poslist.forEach( (x, index) => {
+                if ( (index < poslist.length - 1)
+                     && x[0]+x[1] > poslist[index+1][0] ) {
+                    // oversteps onto next position; shorten
+                    return [ x[0], poslist[index+1][0]-x[0] ];
+                } else {
+                    return [ x[0], x[1] ]; // all ok
+                }
+            });
+
+            //console.log('fieldname =', fieldname, 'poslist =', poslist);
+
+            const docfieldstr = doc[fieldname];
+
+            let showhtml = '';
+            let lastpos = 0;
+
+            for (const pospair of poslist) {
+                if ( (lastpos>0 && ((pospair[0] - lastpos) < 2*context_length))
+                     || (lastpos==0 && (pospair[0] < context_length) ) ) {
+                    // close to previous match (or string start), do not elide
+                    showhtml += escape_html(docfieldstr.substr(lastpos, pospair[0]-lastpos));
+                } else {
+                    if (lastpos != 0) {
+                        showhtml += escape_html(
+                            docfieldstr.substr(lastpos, context_length)
+                        );
+                        showhtml += '…';
+                    }
+                    showhtml += ' …';
+                    showhtml += escape_html(
+                        docfieldstr.substr(pospair[0] - context_length, context_length)
+                    );
+                }
+                showhtml += '<span class="sr-highlight">'
+                    + escape_html( docfieldstr.substr(pospair[0], pospair[1]) )
+                    + '</span>';
+                lastpos = pospair[0] + pospair[1];
+            }
+            if (docfieldstr.length > (lastpos + context_length)) {
+                showhtml += escape_html(
+                    docfieldstr.substr(lastpos, context_length)
+                ) + '…';
+            } else {
+                showhtml += escape_html(
+                    docfieldstr.substr(lastpos)
+                );
+            }
+
+            // field name
+            showhtml += '<span class="search-result-display-field-name">'
+                + escape_html(fieldname)
+                + '</span>';
+
+            const p = document.createElement('div');
+            p.classList.add('search-result-field-display');
+            let fieldnameclsname = fieldname.replace(/[^a-zA-Z0-9_-]/g, "");
+            p.classList.add('search-result-field-display--' + fieldnameclsname);
+            p.innerHTML = showhtml;
+
+            div.appendChild(p);
+        }
+
+        container.appendChild(div);
+    }
+
+}
